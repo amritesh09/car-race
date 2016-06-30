bf9e9763cdafe742062ef24e679c9cefaa01b13a
Amritesh Srivastava <asriva3@L-IDC24KG3QC-M.local>
2016-06-30 12:17:30 +0530

    first commit

---
+++ src/headers.h
@@ -0,0 +1,10 @@
+#include <GL/glut.h>    // Header File For The GLUT Library 
+#include <GL/gl.h>	// Header File For The OpenGL32 Library
+#include <GL/glu.h>	// Header File For The GLu32 Library
+#include <unistd.h>     // Header file for sleeping.
+#include <stdio.h>      // Header file for standard file i/o.
+#include <stdlib.h>     // Header file for malloc/free.
+#include <math.h>       // Header file for trigonometric functions.
+#include <string.h>
+#include "png.c"
+#include "obj.h"
